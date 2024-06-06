1a84b9bd5402d2216832107951a9b488bf52b0c2
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-06 10:04:08 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,495 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-40_overview-1",
+      "name": "SF-40_Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6704697731031626,
+        "pitch": 0.7271150348970821,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6091095181456225,
+          "pitch": 0.39471745446255113,
+          "rotation": 0,
+          "target": "1-sf-40_overview-2"
+        },
+        {
+          "yaw": -0.6669543890393221,
+          "pitch": 0.6574963176356725,
+          "rotation": 0,
+          "target": "4-sf-40_wellhead--pipeline"
+        },
+        {
+          "yaw": 0.5467847980535367,
+          "pitch": 0.7229347371826549,
+          "rotation": 0,
+          "target": "7-sf-40_well-pad-1"
+        },
+        {
+          "yaw": -0.38115579622451534,
+          "pitch": 0.5370372764958411,
+          "rotation": 0,
+          "target": "3-sf-40_sucker-rod-pumping-system"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-40_overview-2",
+      "name": "SF-40_Overview 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9816257791618277,
+        "pitch": 0.8117257392994439,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3598976338719133,
+          "pitch": -0.028937276928260758,
+          "rotation": 0,
+          "target": "0-sf-40_overview-1"
+        },
+        {
+          "yaw": -2.5678011965242913,
+          "pitch": 0.32563480557282354,
+          "rotation": 0,
+          "target": "2-sf-40_well-pad"
+        },
+        {
+          "yaw": 1.9188426054451648,
+          "pitch": 0.4491133187801477,
+          "rotation": 0,
+          "target": "6-sf-40_old-transformer--ohl"
+        },
+        {
+          "yaw": 1.0279353600882786,
+          "pitch": 1.4779927943253135,
+          "rotation": 0,
+          "target": "4-sf-40_wellhead--pipeline"
+        },
+        {
+          "yaw": -1.0445707174228378,
+          "pitch": 0.37760571484617955,
+          "rotation": 0,
+          "target": "7-sf-40_well-pad-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-40_well-pad",
+      "name": "SF-40_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.12541692687142,
+        "pitch": 0.015038824816926422,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9644529419479646,
+          "pitch": -0.3029578782447757,
+          "rotation": 0,
+          "target": "1-sf-40_overview-2"
+        },
+        {
+          "yaw": 0.8636167245308339,
+          "pitch": 0.053573095500754064,
+          "rotation": 0,
+          "target": "3-sf-40_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.05153116614022579,
+          "pitch": -0.02985095194067533,
+          "rotation": 0,
+          "target": "7-sf-40_well-pad-1"
+        },
+        {
+          "yaw": 1.2416677521501018,
+          "pitch": 0.0173000947416746,
+          "rotation": 0,
+          "target": "5-sf-40_sucker-rod-pumping-system-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-40_sucker-rod-pumping-system",
+      "name": "SF-40_Sucker rod pumping system",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.03288226105884817,
+        "pitch": -0.15049368181208678,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.600312582452455,
+          "pitch": 0.15663583568631623,
+          "rotation": 0,
+          "target": "2-sf-40_well-pad"
+        },
+        {
+          "yaw": 0.6896915517954874,
+          "pitch": 0.2633270716743983,
+          "rotation": 0,
+          "target": "5-sf-40_sucker-rod-pumping-system-1"
+        },
+        {
+          "yaw": -0.8610723817049699,
+          "pitch": 0.057067634086472196,
+          "rotation": 0,
+          "target": "4-sf-40_wellhead--pipeline"
+        },
+        {
+          "yaw": -1.9673691074272064,
+          "pitch": 0.011333835977332285,
+          "rotation": 0,
+          "target": "7-sf-40_well-pad-1"
+        },
+        {
+          "yaw": -1.682554128272427,
+          "pitch": -0.4573632861371131,
+          "rotation": 0,
+          "target": "0-sf-40_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-40_wellhead--pipeline",
+      "name": "SF-40_Wellhead & pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.0008430033493951328,
+        "pitch": -0.16438249571512742,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3309101937053747,
+          "pitch": -0.15660906325005897,
+          "rotation": 0,
+          "target": "0-sf-40_overview-1"
+        },
+        {
+          "yaw": 1.3239111677670863,
+          "pitch": -0.736235813406001,
+          "rotation": 0,
+          "target": "1-sf-40_overview-2"
+        },
+        {
+          "yaw": 1.5111614935527342,
+          "pitch": 0.1449891816190778,
+          "rotation": 0,
+          "target": "3-sf-40_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -0.2147943875768341,
+          "pitch": 0.03957032642986036,
+          "rotation": 0,
+          "target": "6-sf-40_old-transformer--ohl"
+        },
+        {
+          "yaw": -2.8166916889602955,
+          "pitch": 0.09189169517031459,
+          "rotation": 0,
+          "target": "7-sf-40_well-pad-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-40_sucker-rod-pumping-system-1",
+      "name": "SF-40_Sucker rod pumping system 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.31012969560628534,
+        "pitch": 0.1250229822826654,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5228542563544618,
+          "pitch": 0.05363751002131245,
+          "rotation": 0,
+          "target": "3-sf-40_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -1.4643209813477345,
+          "pitch": 0.10252934850415762,
+          "rotation": 0,
+          "target": "2-sf-40_well-pad"
+        },
+        {
+          "yaw": 2.225190997662038,
+          "pitch": 0.13258169451217583,
+          "rotation": 0,
+          "target": "6-sf-40_old-transformer--ohl"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-40_old-transformer--ohl",
+      "name": "SF-40_Old transformer & OHL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.09525937848167487,
+        "pitch": -0.10997391960049185,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5707355769027256,
+          "pitch": -0.5493672138123831,
+          "rotation": 0,
+          "target": "1-sf-40_overview-2"
+        },
+        {
+          "yaw": 2.799909015796967,
+          "pitch": 0.03158377688100522,
+          "rotation": 0,
+          "target": "4-sf-40_wellhead--pipeline"
+        },
+        {
+          "yaw": 1.8853685750329703,
+          "pitch": 0.10145448553079817,
+          "rotation": 0,
+          "target": "5-sf-40_sucker-rod-pumping-system-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-40_well-pad-1",
+      "name": "SF-40_Well Pad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.1825285095523679,
+        "pitch": 0.23499690188312528,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8311721946455908,
+          "pitch": 0.14790777731733762,
+          "rotation": 0,
+          "target": "3-sf-40_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 1.3649002906413568,
+          "pitch": 0.12321226847693367,
+          "rotation": 0,
+          "target": "4-sf-40_wellhead--pipeline"
+        },
+        {
+          "yaw": 1.5112480293899635,
+          "pitch": -0.38414867967502175,
+          "rotation": 0,
+          "target": "1-sf-40_overview-2"
+        },
+        {
+          "yaw": -0.6812440327183911,
+          "pitch": -0.4127080276605479,
+          "rotation": 0,
+          "target": "0-sf-40_overview-1"
+        },
+        {
+          "yaw": 2.724961413419626,
+          "pitch": 0.06662035988007453,
+          "rotation": 0,
+          "target": "2-sf-40_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF+40",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
